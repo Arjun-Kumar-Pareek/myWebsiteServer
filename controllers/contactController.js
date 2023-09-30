bbfd269e89fb534aab4e9d3f71310330000ea5be
Arjun-Kumar-Pareek <pareeka8209@gmail.com>
2023-09-30 12:12:58 +0530

    Add mail response in contact us function

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -14,6 +14,8 @@ module.exports.contactUs = async (req, res) => {
             reason: reason
         });
         const saveData = await saveContact.save();
+        const sendMailResponse = helper.sendEmail(email, 'Contact regarding', 'Thank you for contact us',
+            `<p>Thank You <b> ${name} </b> for contact with us.</p> <p>We will respond to you soon.</p>`)
         res.status(200).send({ success: true, message: "Thank you for contact us.", data: saveContact });
 
     } catch (error) {
